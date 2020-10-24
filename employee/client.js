05a3448ee575320085cbc09708dfaed22eda9020
99002758 <72496047+99002758@users.noreply.github.com>
2020-10-24 19:47:28 +0530

    Add files via upload

---
+++ employee/client.js
@@ -0,0 +1,9 @@
+onst app = require("express")();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/Views/index.html");
+})
+
+app.listen(1235, () => {
+    console.log("Client App running at 1235");
+})
